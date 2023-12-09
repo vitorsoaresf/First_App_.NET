7e65e35724e48418bac46c5a1ca49e0906e47e45
Vitor Soares <vitorsoaresf@gmail.com>
2023-12-09 13:34:16 -0300

    feat: implements menu user options

--- First_App_.NET/Program.cs
+++ First_App_.NET/Program.cs
@@ -15,8 +15,17 @@ void ExibirOpcoesDoMenu()
     Console.WriteLine("\nDigite 1 para registrar uma banda");
     Console.WriteLine("Digite 2 para mostrar todas as bandas");
     Console.WriteLine("Digite 3 para avaliar uma banda");
-    Console.WriteLine("Digite 4 para exibir a m[edia de uma banda");
+    Console.WriteLine("Digite 4 para exibir a média de uma banda");
     Console.WriteLine("Digite -1 para sair");
+
+    Console.Write("\nDigite a sua opção: ");
+    string opcaoEscolhida = Console.ReadLine()!;
+    int opcaoEscolhidaNumerica = int.Parse(opcaoEscolhida);
+
+    if(opcaoEscolhidaNumerica == 1)
+    {
+        Console.WriteLine("Você digitou a opção "+ opcaoEscolhida);
+    }
 }
 
 ExibirBoasVindas();
--- First_App_.NET/obj/Release/net8.0/First_App_.NET.AssemblyInfo.cs
+++ First_App_.NET/obj/Release/net8.0/First_App_.NET.AssemblyInfo.cs
@@ -12,9 +12,9 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("First_App_.NET")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+879f4469f48721b40e39ba59223458be2f2f62a3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("First_App_.NET")]
 [assembly: System.Reflection.AssemblyTitleAttribute("First_App_.NET")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
