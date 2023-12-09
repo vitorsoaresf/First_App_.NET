879f4469f48721b40e39ba59223458be2f2f62a3
Vitor Soares <vitorsoaresf@gmail.com>
2023-12-09 13:26:24 -0300

    chore: build first program c#

---
+++ First_App_.NET/Program.cs
@@ -0,0 +1,23 @@
+﻿void ExibirBoasVindas()
+{ 
+    Console.WriteLine(@"
+███████╗██╗██████╗░░██████╗████████╗  ░█████╗░██████╗░██████╗░  ░█████╗░░░░██╗░██╗░
+██╔════╝██║██╔══██╗██╔════╝╚══██╔══╝  ██╔══██╗██╔══██╗██╔══██╗  ██╔══██╗██████████╗
+█████╗░░██║██████╔╝╚█████╗░░░░██║░░░  ███████║██████╔╝██████╔╝  ██║░░╚═╝╚═██╔═██╔═╝
+██╔══╝░░██║██╔══██╗░╚═══██╗░░░██║░░░  ██╔══██║██╔═══╝░██╔═══╝░  ██║░░██╗██████████╗
+██║░░░░░██║██║░░██║██████╔╝░░░██║░░░  ██║░░██║██║░░░░░██║░░░░░  ╚█████╔╝╚██╔═██╔══╝
+╚═╝░░░░░╚═╝╚═╝░░╚═╝╚═════╝░░░░╚═╝░░░  ╚═╝░░╚═╝╚═╝░░░░░╚═╝░░░░░  ░╚════╝░░╚═╝░╚═╝░░░
+");    
+}
+
+void ExibirOpcoesDoMenu()
+{
+    Console.WriteLine("\nDigite 1 para registrar uma banda");
+    Console.WriteLine("Digite 2 para mostrar todas as bandas");
+    Console.WriteLine("Digite 3 para avaliar uma banda");
+    Console.WriteLine("Digite 4 para exibir a m[edia de uma banda");
+    Console.WriteLine("Digite -1 para sair");
+}
+
+ExibirBoasVindas();
+ExibirOpcoesDoMenu();
